75942f24d5b9b53cb2d75fd744e97992f391ee34
c0d3m0nk3y <rickyoswald@gmail.com>
2017-07-19 06:51:17 +0100

    start

---
+++ camera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+	public GameObject player;
+	Vector3 offset;
+	float yRotation;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		BetaMove ();
+	}
+
+	void BetaMove() {
+		Quaternion rotation = Quaternion.Euler (
+			0f,
+			yRotation += Input.GetAxis("Horizontal"),
+			0f
+		);
+
+		transform.rotation = rotation;
+		transform.position = player.transform.position + rotation * new Vector3 (0f, 1f, -3f);
+		transform.LookAt (player.transform.position);
+	}
+		
+
+	void AlphaMove() {
+		transform.LookAt (player.transform);
+		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * 30f;
+		transform.RotateAround (player.transform.position, Vector3.up, x);
+
+		//transform.TransformDirection (Vector3.forward);
+	}
+		
+}
---
+++ player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	Rigidbody rb;
+	public Camera camera;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AlphaMove ();
+	}
+
+	void AlphaMove() {
+		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * 50f;
+		float z = Input.GetAxis ("Vertical") * Time.deltaTime * 50f;
+
+		Vector3 v = camera.transform.forward;
+		v.y = transform.position.y;
+
+		rb.AddForce (v.normalized * z);
+	}
+
+	void BetaMove() {
+	}
+}
